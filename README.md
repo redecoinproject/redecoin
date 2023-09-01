7afb978afd10ec512b72ac2cd44e3edbbe2c0f51
redecoinproject <redecoinproject@gmail.com>
2023-09-01 20:47:53 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+## Faster start node sync
+```
+./geth --syncmode full console
+```
+## Building the source
+
+For prerequisites and detailed build instructions please read the
+
+Building `geth` requires both a Go (version 1.16 or later) and a C compiler. You can install
+them using your favourite package manager. Once the dependencies are installed, run
+
+```shell
+make geth
+```
+
+or, to build the full suite of utilities:
+
+```shell
+make all
+```
+
+## Running `geth`
+
+Minimum:
+
+* CPU with 2+ cores
+* 4GB RAM
+* 1TB free storage space to sync the Mainnet
+* 8 MBit/sec download Internet service
+
+Recommended:
+
+* Fast CPU with 4+ cores
+* 16GB+ RAM
+* High Performance SSD with at least 1TB free space
+* 25+ MBit/sec download Internet service
+
---
+++ params/bootnodes.go
@@ -0,0 +1,59 @@
+// Copyright 2015 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+package params
+
+import "github.com/ethereum/go-ethereum/common"
+
+var MainnetBootnodes = []string{
+"enode://3eb5c62e61743442e392a3de56fda8cee541f56ef2f43cae032521f3ac715fe7393a97e08aea2ef6da06be73cd21396f8b2ed066e29181a1ff5b0519cb19eee2@37.187.131.105:30304",
+}
+var RopstenBootnodes = []string{
+}
+var SepoliaBootnodes = []string{
+}
+var RinkebyBootnodes = []string{
+}
+var GoerliBootnodes = []string{
+}
+var KilnBootnodes = []string{
+}
+var V5Bootnodes = []string{
+}
+
+const dnsPrefix = "enrtree://AKA3AM6LPBYEUDMVNU3BSVQJ5AD45Y7YPOHJLEF6W26QOE4VTUDPE@"
+
+// KnownDNSNetwork returns the address of a public DNS-based node list for the given
+// genesis hash and protocol. See https://github.com/ethereum/discv4-dns-lists for more
+// information.
+func KnownDNSNetwork(genesis common.Hash, protocol string) string {
+	var net string
+	switch genesis {
+	case MainnetGenesisHash:
+		net = "mainnet"
+	case RopstenGenesisHash:
+		net = "ropsten"
+	case RinkebyGenesisHash:
+		net = "rinkeby"
+	case GoerliGenesisHash:
+		net = "goerli"
+	case SepoliaGenesisHash:
+		net = "sepolia"
+	default:
+		return ""
+	}
+	return dnsPrefix + protocol + "." + net + ".ethdisco.net"
+}
